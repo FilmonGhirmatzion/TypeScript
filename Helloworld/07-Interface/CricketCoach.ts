bb22dcc5749924cd5a4097f53d66b99c2a6b0983
FilmonGhirmatzion <FilmonGhirmatzion@gmail.com>
2024-12-20 15:26:34 -0600

    interface added

---
+++ Helloworld/07-Interface/CricketCoach.ts
@@ -0,0 +1,9 @@
+import { Coach } from "./Coach";
+
+
+export class CricketCoach {
+    
+    getDailyWorkout(): string {
+        return "Practice your spin bowling technique";
+    }
+}
---
+++ Helloworld/07-Interface/Driver.ts
@@ -0,0 +1,14 @@
+import { CricketCoach } from './CricketCoach';
+import { GolfCoach } from './GolfCoach';
+
+
+let myCricketCoach = new CricketCoach();
+let myGolfCoach = new GolfCoach();
+
+let theCoaches: CricketCoach[] = [];
+theCoaches.push(myCricketCoach);
+theCoaches.push(myGolfCoach);
+
+for ( let tempCoaches of theCoaches) {
+    console.log(tempCoaches.getDailyWorkout());
+}
